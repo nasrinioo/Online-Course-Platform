68b112c666e3ce0c8c9697a0fd341e768e4d126d
nasrinio <itmohamednasr@gmail.com>
2025-08-04 16:15:10 +0300

    Enhance server startup log message with green color coding for improved visibility.

--- server.js
+++ server.js
@@ -5,5 +5,5 @@ const app = require("./app");
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
-  console.log(`Server is listening on port ${PORT}...`);
+  console.log(`\x1b[32m✓ Server is listening on port ${PORT}...\x1b[0m`);
 });
